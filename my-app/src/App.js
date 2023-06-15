0dce927b7f01d4d18d7065c198c066056d9f8534
Hannah <hannahjahn1024@gmail.com>
2023-06-15 14:42:03 -0700

    added onclick stuffs and temp and op values

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
-import Calculator from "./component/Calculator";
+import { useState } from "react";
+
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  const [temp, setTemp] = useState(0);
+  const [op, setOp] = useState("");
+
+  function percentOp() {
+    setCount(count / 100);
+  }
+  
+  // Function to handle number button clicks
+  function changeCount(value) {
+    setCount(count * 10 + value);
+  };
+
+  function negative() {
+    setCount(count * -1)
+  }
+
+  function clear() {
+    setCount(0);
+    setTemp(0);
+    setOp("");
+  }
+
   // Addition function
   let add = function (a, b) {
     let sum = a + b;
@@ -39,9 +64,60 @@ function App() {
 
   // Equal function
 
+  const debug = () => {
+    console.log("count:" + count);
+    console.log("op:" + op);
+    console.log("temp:" + temp);
+    console.log("-------------");
+  }
+
   return (
     <div className="App">
-      <Calculator />
+      <h1 style=
+        {{
+          color: "red",
+          background: "orange",
+          textAlign: "center"
+        }}>
+          ✌️Hello 🤡Hannah 🧐James 👽Alejandro!
+      </h1>
+      <button onClick={() => debug()}>
+        STATE STATUS
+      </button>
+        <div className="calculatorStyle">
+      <div className="calculatorBackground">
+        <span id="calculatorValue">{count}</span>
+      <div className="calculatorRow">
+        <button onClick={() => clear()}>C</button>
+        <button onClick={() => negative()}>+/-</button>
+        <button onClick={() => percentOp()}>%</button>
+        <button id="calculatorOperator" onClick={() => {setOp(4); setTemp(count)}}>÷</button>
+      </div>
+      <div className="calculatorRow">
+        <button onClick={() => changeCount(7)}>7</button>
+        <button onClick={() => changeCount(8)}>8</button>
+        <button onClick={() => changeCount(9)}>9</button>
+        <button id="calculatorOperator" onClick={() => {setOp(3); setTemp(count)}}>X</button>
+      </div>
+      <div className="calculatorRow">
+        <button onClick={() => changeCount(4)}>4</button>
+        <button onClick={() => changeCount(5)}>5</button>
+        <button onClick={() => changeCount(6)}>6</button>
+        <button id="calculatorOperator" onClick={() => {setOp(2); setTemp(count)}}>-</button>
+      </div>
+      <div className="calculatorRow">
+        <button onClick={() => changeCount(1)}>1</button>
+        <button onClick={() => changeCount(2)}>2</button>
+        <button onClick={() => changeCount(3)}>3</button>
+        <button id="calculatorOperator" onClick={() => {setOp(1); setTemp(count)}}>+</button>
+      </div>
+      <div className="calculatorRow">
+        <button id="zeroButton" onClick={() => changeCount(0)}>0</button>
+        <button>.</button>
+        <button id="calculatorOperator">=</button>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }

e62057ca3acf5514d86e947f6edbd0cd1049567f
bryal07 <bryalejandro07@gmail.com>
2023-06-19 04:53:40 -0700

    Moved the functions operations to a seperate JS and added an equals operations

---
+++ my-app/src/calculatorFunctions.js
@@ -0,0 +1,20 @@
+// Addition function
+export function add(a, b) {
+    return a + b;
+  }
+  
+  // Subtraction function
+  export function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Multiplication function
+  export function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Division function
+  export function divide(a, b) {
+    return a / b;
+  }
+  
